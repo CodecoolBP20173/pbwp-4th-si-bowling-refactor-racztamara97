383c72154611225fc653369ff28416119c84180d
Rácz Tamara <racztamara97@gmail.com>
2017-12-02 21:00:09 +0100

    Add the first comments (in hungarian language)

--- bowling.py
+++ bowling.py
@@ -7,28 +7,27 @@ def score(game):
             result += 10 - last
         else:
             result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
         if frame < 10  and get_value(game[i]) == 10:
             if game[i] == '/':
                 result += get_value(game[i+1])
             elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
+                result += get_value(game[i+1])  # ha x-et gurított, akkor hozzáadom az eredményhez a kövi gurítást
+                if game[i+2] == '/':    # ha x után a 2. gurításommal tarolok, akkor +10pont jár (-amit legutóbb adtam hozzá)
                     result += 10 - get_value(game[i+1])
                 else:
-                    result += get_value(game[i+2])
-        last = get_value(game[i])
-        if not in_first_half:
+                    result += get_value(game[i+2])  # ha nem tarolok, akkor hozzáadom a második gurítást is
+        last = get_value(game[i])   # last = utolsó gurítás
+        if not in_first_half:   # ha a körön belül nem az 1. gurításnál tartunk, akkor a körök sorszámát növeljük eggyel
             frame += 1
-        if in_first_half == True:
+        if in_first_half is True:   # ha az 1. gurításnál tartottunk, akkor ezt módosítjuk, mert ugye utána már a 2. gurítás jön
             in_first_half = False
         else:
-            in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
+            in_first_half = True    # ha a 2. gurításnál tartottunk, akkor ezt az 1. gurításra változtatjuk
+        if game[i] == 'X' or game[i] == 'x':    # ha x-et gurítok, akkor nem lesz 2. gurításom, így a körök számát növelem eggyel
             in_first_half = True
             frame += 1
     return result
+
 
 def get_value(char):
     if char == '1' or char == '2' or char == '3' or \
