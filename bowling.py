d367ab511ef8cdb61526bd65d6954ee0e2fdde68
Rácz Tamara <racztamara97@gmail.com>
2017-12-03 22:30:03 +0100

    Comments added (in English)

--- bowling.py
+++ bowling.py
@@ -1,4 +1,7 @@
 def score(game):
+    '''
+    This function counts the score.
+    '''
     result = 0
     frame = 1
     in_first_half = True
@@ -9,32 +12,41 @@ def score(game):
             result += get_value(game[i])
         if frame < 10  and get_value(game[i]) == 10:
             result = check_spare_or_strike(game, result, i)
-        last = get_value(game[i])   # last = utolsó gurítás
-        if not in_first_half:   # ha a körön belül nem az 1. gurításnál tartunk, akkor a körök sorszámát növeljük eggyel
+        last = get_value(game[i])
+        if not in_first_half:
             frame += 1
-        if in_first_half is True:   # ha az 1. gurításnál tartottunk, akkor ezt módosítjuk, mert ugye utána már a 2. gurítás jön
+        if in_first_half is True:
             in_first_half = False
         else:
-            in_first_half = True    # ha a 2. gurításnál tartottunk, akkor ezt az 1. gurításra változtatjuk
-        if game[i] == 'X' or game[i] == 'x':    # ha x-et gurítok, akkor nem lesz 2. gurításom, így a körök számát növelem eggyel
+            in_first_half = True
+        if game[i] == 'X' or game[i] == 'x':
             in_first_half = True
             frame += 1
     return result
 
 
 def check_spare_or_strike(game, result, i):
+    '''
+    This function checks that tha last roll was spare/strike or not.
+    If you rolled /, it adds your roll to the score.
+    If you rolled X, it adds your roll to the score.
+        If you rolled X and the next roll is /, than it adds 10 points to the score and substracts the previous roll, else it gives your roll to the score.
+    '''
     if game[i] == '/':
         result += get_value(game[i+1])
     elif game[i] == 'X' or game[i] == 'x':
-        result += get_value(game[i+1])  # ha x-et gurított, akkor hozzáadom az eredményhez a kövi gurítást
-        if game[i+2] == '/':    # ha x után a 2. gurításommal tarolok, akkor +10pont jár (-amit legutóbb adtam hozzá)
+        result += get_value(game[i+1])
+        if game[i+2] == '/':
             result += 10 - get_value(game[i+1])
         else:
-            result += get_value(game[i+2])  # ha nem tarolok, akkor hozzáadom a második gurítást is
+            result += get_value(game[i+2])
     return result
 
 
 def get_value(char):
+    '''
+    This function checks the inputs.
+    '''
     if char == '1' or char == '2' or char == '3' or \
        char == '4' or char == '5' or char == '6' or \
        char == '7' or char == '8' or char == '9':
